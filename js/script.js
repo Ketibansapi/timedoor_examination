3a70628a56a510f062d9d75a8bddee85ff4cd0fc
Ketibansapi <deary2706@gmail.com>
2020-07-31 16:28:50 +0700

    adding functions for each toppings

--- js/script.js
+++ js/script.js
@@ -39,42 +39,207 @@ function getToppings(runningTotal,text1,text2) {
 		if (selectedToppings === "Avocado Topping") {
 			toppingsTotal = 1;
 		}
-		if (selectedToppings === "Broccoli Topping") {
-			toppingsTotal = 1;
-		}
-		if (selectedToppings === "Onions Topping") {
-			toppingsTotal = 1;
-		}
-		if (selectedToppings === "Zucchini Topping") {
-			toppingsTotal = 1;
-		}
-		if (selectedToppings === "Lobster Topping") {
-			toppingsTotal = 2;
-		}
-		if (selectedToppings === "Oyster Topping") {
-			toppingsTotal = 2;
-		}
-		if (selectedToppings === "Salmon Topping") {
-			toppingsTotal = 2;
-		}
-		if (selectedToppings === "Tuna Topping") {
-			toppingsTotal = 2;
-		}
-		if (selectedToppings === "Bacon Topping") {
-			toppingsTotal = 3;
-		}
-		if (selectedToppings === "Duck Topping") {
-			toppingsTotal = 3;
-		}
-		if (selectedToppings === "Ham Topping") {
-			toppingsTotal = 3;
-		}
-		if (selectedToppings === "Sausage Topping") {
-			toppingsTotal = 3;
-		}
 	}
 	runningTotal = (runningTotal + toppingsTotal);
 	text2 = text2 + toppingsTotal + "<br>";
+	getBroccoli(runningTotal,text1,text2);
+};
+
+function getBroccoli(runningTotal,text1,text2) {
+	var broccoliTotal = 0;
+	var selectedBroccoli;
+	var broccoliArray = document.getElementsByClassName("broccoli");
+	for (var j = 0; j < broccoliArray.length; j++) {
+		if (broccoliArray[j].checked) {
+			selectedBroccoli = broccoliArray[j].value;
+			text1 = text1 + selectedBroccoli + "<br>";
+		}
+		if (selectedBroccoli === "Broccoli Topping") {
+			broccoliTotal = 1;
+		}
+	}
+	runningTotal = (runningTotal + broccoliTotal);
+	text2 = text2 + broccoliTotal + "<br>";
+	getOnions(runningTotal,text1,text2);
+};
+
+function getOnions(runningTotal,text1,text2) {
+	var onionsTotal = 0;
+	var selectedOnions;
+	var onionsArray = document.getElementsByClassName("onions");
+	for (var j = 0; j < onionsArray.length; j++) {
+		if (onionsArray[j].checked) {
+			selectedOnions = onionsArray[j].value;
+			text1 = text1 + selectedOnions + "<br>";
+		}
+		if (selectedOnions === "Onions Topping") {
+			onionsTotal = 1;
+		}
+	}
+	runningTotal = (runningTotal + onionsTotal);
+	text2 = text2 + onionsTotal + "<br>";
+	getZucchini(runningTotal,text1,text2);
+};
+
+function getZucchini(runningTotal,text1,text2) {
+	var zucchiniTotal = 0;
+	var selectedZucchini;
+	var zucchiniArray = document.getElementsByClassName("zucchini");
+	for (var j = 0; j < zucchiniArray.length; j++) {
+		if (zucchiniArray[j].checked) {
+			selectedZucchini = zucchiniArray[j].value;
+			text1 = text1 + selectedZucchini + "<br>";
+		}
+		if (selectedZucchini === "Zucchini Topping") {
+			zucchiniTotal = 1;
+		}
+	}
+	runningTotal = (runningTotal + zucchiniTotal);
+	text2 = text2 + zucchiniTotal + "<br>";
+	getLobster(runningTotal,text1,text2);
+};
+
+function getLobster(runningTotal,text1,text2) {
+	var lobsterTotal = 0;
+	var selectedLobster;
+	var lobsterArray = document.getElementsByClassName("lobster");
+	for (var j = 0; j < lobsterArray.length; j++) {
+		if (lobsterArray[j].checked) {
+			selectedLobster = lobsterArray[j].value;
+			text1 = text1 + selectedLobster + "<br>";
+		}
+		if (selectedLobster === "Lobster Topping") {
+			lobsterTotal = 2;
+		}
+	}
+	runningTotal = (runningTotal + lobsterTotal);
+	text2 = text2 + lobsterTotal + "<br>";
+	getOyster(runningTotal,text1,text2);
+};
+
+function getOyster(runningTotal,text1,text2) {
+	var oysterTotal = 0;
+	var selectedOyster;
+	var oysterArray = document.getElementsByClassName("oyster");
+	for (var j = 0; j < oysterArray.length; j++) {
+		if (oysterArray[j].checked) {
+			selectedOyster = oysterArray[j].value;
+			text1 = text1 + selectedOyster + "<br>";
+		}
+		if (selectedOyster === "Oyster Topping") {
+			oysterTotal = 2;
+		}
+	}
+	runningTotal = (runningTotal + oysterTotal);
+	text2 = text2 + oysterTotal + "<br>";
+	getSalmon(runningTotal,text1,text2);
+};
+
+function getSalmon(runningTotal,text1,text2) {
+	var salmonTotal = 0;
+	var selectedSalmon;
+	var salmonArray = document.getElementsByClassName("salmon");
+	for (var j = 0; j < salmonArray.length; j++) {
+		if (salmonArray[j].checked) {
+			selectedSalmon = salmonArray[j].value;
+			text1 = text1 + selectedSalmon + "<br>";
+		}
+		if (selectedSalmon === "Salmon Topping") {
+			salmonTotal = 2;
+		}
+	}
+	runningTotal = (runningTotal + salmonTotal);
+	text2 = text2 + salmonTotal + "<br>";
+	getTuna(runningTotal,text1,text2);
+};
+
+function getTuna(runningTotal,text1,text2) {
+	var tunaTotal = 0;
+	var selectedTuna;
+	var tunaArray = document.getElementsByClassName("tuna");
+	for (var j = 0; j < tunaArray.length; j++) {
+		if (tunaArray[j].checked) {
+			selectedTuna = tunaArray[j].value;
+			text1 = text1 + selectedTuna + "<br>";
+		}
+		if (selectedTuna === "Tuna Topping") {
+			tunaTotal = 2;
+		}
+	}
+	runningTotal = (runningTotal + tunaTotal);
+	text2 = text2 + tunaTotal + "<br>";
+	getBacon(runningTotal,text1,text2);
+};
+
+function getBacon(runningTotal,text1,text2) {
+	var baconTotal = 0;
+	var selectedBacon;
+	var baconArray = document.getElementsByClassName("bacon");
+	for (var j = 0; j < baconArray.length; j++) {
+		if (baconArray[j].checked) {
+			selectedBacon = baconArray[j].value;
+			text1 = text1 + selectedBacon + "<br>";
+		}
+		if (selectedBacon === "Bacon Topping") {
+			baconTotal = 3;
+		}
+	}
+	runningTotal = (runningTotal + baconTotal);
+	text2 = text2 + baconTotal + "<br>";
+	getDuck(runningTotal,text1,text2);
+};
+
+function getDuck(runningTotal,text1,text2) {
+	var duckTotal = 0;
+	var selectedDuck;
+	var duckArray = document.getElementsByClassName("duck");
+	for (var j = 0; j < duckArray.length; j++) {
+		if (duckArray[j].checked) {
+			selectedDuck = duckArray[j].value;
+			text1 = text1 + selectedDuck + "<br>";
+		}
+		if (selectedDuck === "Duck Topping") {
+			duckTotal = 3;
+		}
+	}
+	runningTotal = (runningTotal + duckTotal);
+	text2 = text2 + duckTotal + "<br>";
+	getHam(runningTotal,text1,text2);
+};
+
+function getHam(runningTotal,text1,text2) {
+	var hamTotal = 0;
+	var selectedHam;
+	var hamArray = document.getElementsByClassName("ham");
+	for (var j = 0; j < hamArray.length; j++) {
+		if (hamArray[j].checked) {
+			selectedHam = hamArray[j].value;
+			text1 = text1 + selectedHam + "<br>";
+		}
+		if (selectedHam === "Ham Topping") {
+			hamTotal = 3;
+		}
+	}
+	runningTotal = (runningTotal + hamTotal);
+	text2 = text2 + hamTotal + "<br>";
+	getSausage(runningTotal,text1,text2);
+};
+
+function getSausage(runningTotal,text1,text2) {
+	var sausageTotal = 0;
+	var selectedSausage;
+	var sausageArray = document.getElementsByClassName("sausage");
+	for (var j = 0; j < sausageArray.length; j++) {
+		if (sausageArray[j].checked) {
+			selectedSausage = sausageArray[j].value;
+			text1 = text1 + selectedSausage + "<br>";
+		}
+		if (selectedSausage === "Sausage Topping") {
+			sausageTotal = 3;
+		}
+	}
+	runningTotal = (runningTotal + sausageTotal);
+	text2 = text2 + sausageTotal + "<br>";
 	getItemSize(runningTotal,text1,text2);
 };
 
@@ -106,36 +271,36 @@ function getCheese(runningTotal,text1,text2) {
 	}
 	runningTotal = (runningTotal + cheeseTotal);
 	text2 = text2 + cheeseTotal + "<br>";
-	getMeat(runningTotal,text1,text2);
+	getTopping(runningTotal,text1,text2);
 };
 
-function getMeat(runningTotal,text1,text2) {
-	var meatTotal = 0;
-	var selectedMeat = [];
-	var meatArray = document.getElementsByClassName("meat");
-	for (var j = 0; j < meatArray.length; j++) {
-		if (meatArray[j].checked) {
-			selectedMeat.push(meatArray[j].value);
+function getTopping(runningTotal,text1,text2) {
+	var toppingTotal = 0;
+	var selectedTopping = [];
+	var toppingArray = document.getElementsByClassName("topping");
+	for (var j = 0; j < toppingArray.length; j++) {
+		if (toppingArray[j].checked) {
+			selectedTopping.push(toppingArray[j].value);
 		}
 	}
-	var meatCount = selectedMeat.length;
-	if (meatCount > 1) {
-		meatTotal = (meatCount - 1);
+	var toppingCount = selectedTopping.length;
+	if (toppingCount > 1) {
+		toppingTotal = (toppingCount - 1);
 	} else {
-		meatTotal = 0;
+		toppingTotal = 0;
 	}
-	runningTotal = (runningTotal + meatTotal);
-	for (var j = 0; j < selectedMeat.length; j++) {
-		text1 = text1+selectedMeat[j]+"<br>";
-		if (meatCount <= 1) {
+	runningTotal = (runningTotal + toppingTotal);
+	for (var j = 0; j < selectedTopping.length; j++) {
+		text1 = text1+selectedTopping[j]+"<br>";
+		if (toppingCount <= 1) {
 				text2 = text2 + 0 + "<br>";
-				meatCount = meatCount - 1;
-			} else if (meatCount == 2) {
+				toppingCount = toppingCount - 1;
+			} else if (toppingCount == 2) {
 				text2 = text2 + 1 + "<br>";
-				meatCount = meatCount - 1;
+				toppingCount = toppingCount - 1;
 			} else {
 				text2 = text2 + 1 + "<br>";
-				meatCount = meatCount - 1;
+				toppingCount = toppingCount - 1;
 			}
 	}
 	getVeggie(runningTotal,text1,text2);
